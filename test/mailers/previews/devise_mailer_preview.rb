942ddd8847fa7fb6f12022fbc42fa3e81d6117fe
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2025-04-26 18:33:07 +0200

    add confirmation instructions mailer preview

--- test/mailers/previews/devise_mailer_preview.rb
+++ test/mailers/previews/devise_mailer_preview.rb
@@ -3,4 +3,9 @@ class DeviseMailerPreview < ActionMailer::Preview
     user = FactoryBot.build_stubbed(:user)
     Devise::Mailer.reset_password_instructions(user, "fake-token", {})
   end
+
+  def confirmation_instructions
+    user = FactoryBot.build_stubbed(:user)
+    Devise::Mailer.confirmation_instructions(user, "fake-token", {})
+  end
 end
