a4e33f0e3b854a3d06076c9dc222d71a51deda7c
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2025-04-27 14:06:38 +0200

    setup font awesome

---
+++ app/javascript/fontawesome.js
@@ -0,0 +1,8 @@
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+
+import { faCamera, faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+
+library.add(faCamera, faHeart, faTwitter);
+
+dom.watch();
