ebea743f419712ad0fb5b724817b7efc9380773e
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2025-04-30 23:16:28 +0200

    add user#show

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,12 @@
+class Admin::UsersController < Admin::BaseController
+  before_action :set_user, only: :show
+
+  def show
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
