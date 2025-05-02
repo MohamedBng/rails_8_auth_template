23415d17410710569a95d2e7965adc06f1199db0
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2025-05-02 20:45:26 +0200

    add destroy user action

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,25 @@
 class Admin::UsersController < Admin::BaseController
-  before_action :set_user, only: :show
+  before_action :set_user, only: [:show, :destroy]
+  load_and_authorize_resource class: 'User'
 
   def show
+  end
+
+  def destroy
+    @user.destroy
+
+    if @user.errors.any?
+      flash[:error] = "Failed to delete user: #{@user.errors.full_messages.join(', ')}"
+      redirect_to admin_user_path(@user)
+      return
+    end
+
+    flash[:success] = "User deleted successfully."
+
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.replace("main", template: "admin/dashboard/index") }
+      format.html { redirect_to admin_dashboard_index_path }
+    end
   end
 
   private
