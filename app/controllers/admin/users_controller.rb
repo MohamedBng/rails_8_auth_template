376fbdef132a5e53dad639448a67cef41f5fcd3c
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2025-05-10 19:56:03 +0200

    add create method

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -13,6 +13,24 @@ class Admin::UsersController < Admin::BaseController
   def new
   end
 
+  def create
+    uploaded_io = params[:user].delete(:profile_image)
+    @user = User.new(new_user_params)
+
+    set_random_password(@user)
+    @user.skip_confirmation!
+
+    if @user.save
+      assign_profile_image(@user, uploaded_io) if uploaded_io
+      @user.send_reset_password_instructions
+      flash[:success] = t("admin.users.create.success")
+      redirect_to admin_user_path(@user)
+    else
+      flash.now[:error] = @user.errors.full_messages.to_sentence
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def edit
     render turbo_stream: turbo_stream.replace(
       "personal_info_user_#{@user.id}",
@@ -67,11 +85,34 @@ class Admin::UsersController < Admin::BaseController
 
   private
 
+  def set_random_password(user)
+    random_password = Devise.friendly_token.first(20)
+    user.password = user.password_confirmation = random_password
+  end
+
+  def assign_profile_image(user, uploaded_io)
+    File.open(uploaded_io.tempfile.path, "rb") do |file|
+      user.profile_image_attacher.assign(
+        file,
+        metadata: {
+          filename:     uploaded_io.original_filename,
+          content_type: uploaded_io.content_type
+        }
+      )
+      user.save!
+    end
+  end
+
   def set_user
     @user = User.find(params[:id])
   end
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :profile_image)
+  end
+
+  def new_user_params
+    params.require(:user)
+          .permit(:first_name, :last_name, :email, role_ids: [])
   end
 end
