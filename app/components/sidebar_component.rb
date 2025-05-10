326ef31244f1c732956bd5f1ed947a90c5da349b
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2025-05-10 20:51:40 +0200

    restrict link in sidebar

--- app/components/sidebar_component.rb
+++ app/components/sidebar_component.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class SidebarComponent < ViewComponent::Base
+  def initialize(current_user:)
+    @current_user = current_user
+  end
+
   erb_template <<-ERB
     <aside class="bg-base-100 flex flex-col transition-all duration-300 w-20" data-controller="sidebar" data-sidebar-expanded-class="w-64" data-sidebar-collapsed-class="w-20">
       <div class="flex-1 p-4 space-y-6">
@@ -10,17 +14,25 @@ class SidebarComponent < ViewComponent::Base
         <% end %>
 
         <nav class="space-y-4">
-          <%= link_to admin_dashboard_index_path, data: { turbo_frame: "main", turbo_action: "advance" }, class: 'flex items-center gap-4 hover:bg-base-200 p-2 rounded' do %>
-            <i class="fas fa-chart-line"></i>
-            <span data-sidebar-target="label" class="hidden"><%= t("sidebar.dashboard") %></span>
+          <% if @current_user.has_permission?("read_dashboard") %>
+            <%= link_to admin_dashboard_index_path, data: { turbo_frame: "main", turbo_action: "advance" }, class: 'flex items-center gap-4 hover:bg-base-200 p-2 rounded' do %>
+              <i class="fas fa-chart-line"></i>
+              <span data-sidebar-target="label" class="hidden"><%= t("sidebar.dashboard") %></span>
+            <% end %>
           <% end %>
-          <%= link_to admin_users_path, data: { turbo_frame: "main", turbo_action: "advance" }, class: 'flex items-center gap-4 hover:bg-base-200 p-2 rounded' do %>
-            <i class="fas fa-users"></i>
-            <span data-sidebar-target="label" class="hidden"><%= t("sidebar.users") %></span>
+
+          <% if @current_user.has_permission?("read_user") %>
+            <%= link_to admin_users_path, data: { turbo_frame: "main", turbo_action: "advance" }, class: 'flex items-center gap-4 hover:bg-base-200 p-2 rounded' do %>
+              <i class="fas fa-users"></i>
+              <span data-sidebar-target="label" class="hidden"><%= t("sidebar.users") %></span>
+            <% end %>
           <% end %>
-          <%= link_to admin_roles_path, data: { turbo_frame: "main", turbo_action: "advance" }, class: 'flex items-center gap-4 hover:bg-base-200 p-2 rounded' do %>
-            <i class="fas fa-user-shield"></i>
-            <span data-sidebar-target="label" class="hidden"><%= t("sidebar.roles") %></span>
+
+          <% if @current_user.has_permission?("read_role") %>
+            <%= link_to admin_roles_path, data: { turbo_frame: "main", turbo_action: "advance" }, class: 'flex items-center gap-4 hover:bg-base-200 p-2 rounded' do %>
+              <i class="fas fa-user-shield"></i>
+              <span data-sidebar-target="label" class="hidden"><%= t("sidebar.roles") %></span>
+            <% end %>
           <% end %>
         </nav>
       </div>
