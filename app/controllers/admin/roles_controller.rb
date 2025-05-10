2f0cdbb3cbef4fa551871fc176be21fc31db1305
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2025-05-10 21:12:15 +0200

    add  v1 index view

--- app/controllers/admin/roles_controller.rb
+++ app/controllers/admin/roles_controller.rb
@@ -2,6 +2,8 @@ class Admin::RolesController < Admin::BaseController
   load_and_authorize_resource
 
   def index
-    @roles = Role.all
+    @roles = Role.left_joins(:users)
+             .select('roles.*, COUNT(users.id) AS users_count')
+             .group('roles.id')
   end
 end
