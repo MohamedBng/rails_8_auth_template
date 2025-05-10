1513ebe997269139c6983b295f032e139faa4b9b
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2025-05-10 16:27:52 +0200

    user i18n on error msg image uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,10 +6,13 @@ class ImageUploader < Shrine
   plugin :derivatives, create_on_promote: true
   plugin :store_dimensions
 
+  VALID_MIME_TYPES = %w[image/jpeg image/png]
+  VALID_EXTENSIONS = %w[jpg jpeg png]
+
   Attacher.validate do
-    validate_max_size 5 * 1024 * 1024, message: "is too large (max is 5 MB)"
-    validate_mime_type %w[image/jpeg image/png], message: "is not a valid image type"
-    validate_extension_inclusion %w[jpg jpeg png], message: "is not a valid image extension"
+    validate_max_size 5 * 1024 * 1024, message: I18n.t('errors.image.file_size', max_size: 5)
+    validate_mime_type VALID_MIME_TYPES, message: I18n.t('errors.image.mime_type', valid_types: VALID_MIME_TYPES.join(', '))
+    validate_extension_inclusion VALID_EXTENSIONS, message: I18n.t('errors.image.extension', valid_extensions: VALID_EXTENSIONS.join(', '))
   end
 
   Attacher.derivatives do |original|
