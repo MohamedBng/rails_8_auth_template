79dbda0ff4dd50f3aeed6d2d1796a574d16d6e82
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2025-05-11 11:45:49 +0200

    add decorator for role badge

---
+++ app/decorators/role_decorator.rb
@@ -0,0 +1,12 @@
+class RoleDecorator < Draper::Decorator
+  delegate_all
+
+  def badge
+    bg_color = object.color.presence || "#6c757d" # Default to neutral grey
+    text_color = h.ideal_text_color(bg_color)
+
+    h.content_tag :span, class: "badge badge-soft", style: "background-color: #{bg_color}; color: #{text_color};" do
+      object.name.titleize
+    end
+  end
+end
