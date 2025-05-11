89acb59a607ffcde4ae1c17a6673a092ab51cdf1
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2025-05-11 09:34:01 +0200

    add scope with users count

--- app/controllers/admin/roles_controller.rb
+++ app/controllers/admin/roles_controller.rb
@@ -2,8 +2,6 @@ class Admin::RolesController < Admin::BaseController
   load_and_authorize_resource
 
   def index
-    @roles = Role.left_joins(:users)
-             .select('roles.*, COUNT(users.id) AS users_count')
-             .group('roles.id')
+    @roles = Role.with_users_count
   end
 end
