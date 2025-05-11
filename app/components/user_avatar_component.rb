531c8b4b0e8f220ed0891dad509364532e22722b
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2025-05-11 10:14:58 +0200

    use decorator full name on user avatar component

--- app/components/user_avatar_component.rb
+++ app/components/user_avatar_component.rb
@@ -10,17 +10,13 @@ class UserAvatarComponent < ViewComponent::Base
     @user.online? ? "avatar-online" : "avatar-offline"
   end
 
-  def full_name
-    [ @user.first_name, @user.last_name ].join(" ")
-  end
-
   erb_template <<-ERB
     <div class="avatar <%= avatar_status_class %>">
       <div class="rounded-full flex items-center justify-center overflow-hidden" style="width: <%= @size %>px; height: <%= @size %>px;">
         <% if @user.profile_image.present? %>
-          <%= image_tag @user.profile_image_url(:thumbnail), alt: full_name, class: "w-full h-full object-cover" %>
+          <%= image_tag @user.profile_image_url(:thumbnail), alt: @user.decorate.full_name, class: "w-full h-full object-cover" %>
         <% else %>
-          <%= image_tag("avatar.png", alt: full_name, class: "w-full h-full object-cover") %>
+          <%= image_tag("avatar.png", alt: @user.decorate.full_name, class: "w-full h-full object-cover") %>
         <% end %>
       </div>
     </div>
