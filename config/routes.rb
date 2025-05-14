dfab8dbd58b0f321083e8b3c167205f675440aa6
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2025-05-14 05:15:20 +0200

    add method destroy on userrolescontroller

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
       end
     end
     resources :roles, only: [ :index, :new, :create, :show, :update, :edit ] do
-      resources :user_roles, only: [ :create, :new ], module: :roles
+      resources :user_roles, only: [ :create, :new, :destroy ], module: :roles
     end
   end
 
