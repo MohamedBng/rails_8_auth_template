c0e2d66be84b04cc01af711f1a2050ab17652aed
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2025-05-15 18:38:16 +0200

    add address fiels and phone to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_09_000253) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_15_111225) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -60,6 +60,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_09_000253) do
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
     t.jsonb "profile_image_data"
+    t.string "phone"
+    t.string "street"
+    t.string "postal_code"
+    t.string "city"
+    t.string "country"
+    t.float "latitude"
+    t.float "longitude"
+    t.text "description"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
